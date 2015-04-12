27cb74ff3a27c03a1f17d1d791219cdd0b64ae12
Alessandro <alessandroempire@gmail.com>
2015-04-12 11:45:50 -0430

    pregunta 1

--- Tarea1/pregunta1.hs
+++ Tarea1/pregunta1.hs
@@ -1,11 +1,10 @@
 import Data.List 
 
 mydropWhile :: (a -> Bool) -> [a] -> [a]
-mydropWhile f xs = foldr dropper [] xs 
-    where dropper x [] = if f x then []
-                                    else [x] 
-          dropper x xs = if f x then xs
+mydropWhile f xs =  foldr (($!) dropper) [] xs 
+    where dropper x xs = if f x then xs
                                 else (x : xs)
 
 myfoldl :: (a -> b -> a) -> a -> [b] -> a
-myfoldl = undefined
+myfoldl f a bs =  (foldr step id bs) a
+    where step x g a = g (f a x)
