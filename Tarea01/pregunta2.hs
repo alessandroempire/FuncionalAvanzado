6914a3b59a16438c2ad9c75a0e3032ebc547896c
Alessandro <alessandroempire@gmail.com>
2015-04-14 22:20:34 -0430

    pregunta2

--- Tarea01/pregunta2.hs
+++ Tarea01/pregunta2.hs
@@ -6,10 +6,20 @@ data Dream b a = Dream a
                | Nightmare b
                deriving (Show)
 
---Instancia Functor
+dreamMap :: (b -> c) -> Dream b a -> Dream c a
+dreamMap f (Dream a)     = Dream a
+dreamMap f (Limbo (b,a)) = Limbo(f b, a)
+dreamMap f (Within a xs) = Within a $ fmap (dreamMap f) xs
+dreamMap f (Nightmare b) = Nightmare $ f b
+
+--newtype Sueno b = Dream
+
+--Instancia Functor sobre b
+--instance Functor (Dream b a)  where 
+--    fmap = dreamMap
 
 --Instancia Foldable
-instance Foldable Dream b a where 
-foldMap 
+--instance Foldable Dream b a where 
+--foldMap 
 
-foldr
+--foldr
