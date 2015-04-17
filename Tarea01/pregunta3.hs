3f95220b2ac3317a6c7ede2854045580ecf20fac
Alessandro <alessandroempire@gmail.com>
2015-04-16 23:04:40 -0430

    pregunta3

--- Tarea01/pregunta3.hs
+++ Tarea01/pregunta3.hs
@@ -1,3 +1,12 @@
-import qualified        Data.Map as Map
+import Data.Map as M
+import Data.Sequence as S
+import Data.Monoid
 
+newtype MultiMap k v = MultiMap (M.Map k (S.Seq v))
+    deriving (Show)
 
+instance Ord k => Monoid (MultiMap k v) where
+    mempty = MultiMap $ M.empty
+    mappend (MultiMap a) (MultiMap b) = MultiMap $ M.unionWith mappend a b 
+
+--Ejemplo
